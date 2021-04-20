cae128992b194bcc30132088d8cf52752bc07261
mustafaguldag <guldag.mustafaa@gmail.com>
2021-04-20 16:12:50 +0300

    commit tree cdec71708f409e6bea18ad610a65ada250efafa9

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards(stringName ,eventNAme){
+    let newArr = [];
+    
+    for (let i =0; i < stringName.length; i++) {
+        let  newStr = `Thank you, ${stringName[i]}, for the wonderful ${eventNAme} gift!` ;
+        newArr.push(newStr)
+    
+    }
+    
+    return newArr ;
+}
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(number){
+    
+    while (number >= 0) { 
+    console.log(number);
+    number-- ;  
+}
+}
+countDown();
